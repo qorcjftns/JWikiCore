b28709433967bd26ae45f6b055f959bab1139419
maskai <qorcjftns2@gmail.com>
2020-07-02 19:49:28 -0400

    removed unused imports

--- src/dev/cheulsoon/jwikicore/Main.java
+++ src/dev/cheulsoon/jwikicore/Main.java
@@ -1,10 +1,5 @@
 package dev.cheulsoon.jwikicore;
 
-import dev.cheulsoon.jwikicore.composer.WikiComposer;
-import dev.cheulsoon.jwikicore.grammar.WikiGrammar;
-import dev.cheulsoon.jwikicore.parser.ParsedComponent;
-import dev.cheulsoon.jwikicore.parser.WikiParser;
-
 public class Main {
 	
 	public static void main(String[] args) {
